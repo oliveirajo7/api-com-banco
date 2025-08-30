6052c587a50e99060018385bffb12d224ab165b2
Joaquim <joaquimoliart123@gmail.com>
2025-08-30 03:37:19 -0300

    feat: instalacao do prisma

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get('/usuarios', (req, res) => {
-  res.send('Lista de usuários');
+const usuarios = [];
+
+app.post('/usuarios', (req, res) => {
+
+  usuarios.push(req.body);
+
+  res.status(201).send('Deu certo');
+
 });
 
-app.listen(3000)
+
+app.get('/usuarios', (req, res) => {
+  res.status(200).json(usuarios);
+});
+
+
+app.listen(3000);
