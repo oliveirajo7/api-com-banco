c9d284a5788a6ee12ae5e691b66fd11818a4075a
Joaquim <joaquimoliart123@gmail.com>
2025-08-31 13:57:21 -0300

    Modificação: Schema do prisma configuracoes do json

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-import express from "express";
+import express from "express"
+import { PrismaClient } from './generated/prisma'
 
 const app = express();
+const prisma = new PrismaClient()
+
 app.use(express.json());
 
 const usuarios = [];
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
-  usuarios.push(req.body);
-
+  await prisma.user.create({
+    data: {
+      nome: req.body.nome,
+      email: req.body.email
+    }
+  })
+ 
   res.status(201).send('Deu certo');
 
 });
